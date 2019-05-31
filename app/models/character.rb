93d3ca028276d2f51ddb2723eb302a7f9c06d741
Ethan Sagin <ethansagin@gmail.com>
2019-05-31 16:08:31 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,7 +3,7 @@ class Character < ActiveRecord::Base
   belongs_to :show 
   
   def say_that_thing_you_say
-    self.catchphrase
+    "3#{self.name} always says: #{self.catchphrase}"
   end
   
 end
