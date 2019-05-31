256c772e72145bd31d7a434edb72f3a7093cd89a
Ethan Sagin <ethansagin@gmail.com>
2019-05-31 16:12:59 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_attributes_to_show.rb
+++ db/migrate/007_add_attributes_to_show.rb
@@ -2,7 +2,8 @@ class AddAttributesToShow < ActiveRecord::Migration[4.2]
 
   def change
     add_column :shows, :genre, :string 
-    add_column :shows, :
+    add_column :shows, :day, :string 
+    add_column :shows, :season, :string
   end
   
 end
